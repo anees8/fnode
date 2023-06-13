bb6daaa48076c90752b881b97fb4f26b65206f8c
anees8 <aneesahmed8484@gmail.com>
2023-06-13 13:25:39 +0530

    13062023

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const fileUpload = require('express-fileupload'); 
 const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
 const morgan = require('morgan');
 const cors = require('cors');
+const helmet=require('helmet');
 require('dotenv').config();
 const app = express();
+app.use(helmet());
 app.use(cors({ origin: 'http://localhost:5173' }));
-
 const port = process.env.PORT || 3000; // You can use any valid port number here
 
 app.use(express.urlencoded({
@@ -21,7 +24,7 @@ const EmployeeRoute = require('./routes/employee');
 const ProductRoute = require('./routes/product');
 const ImapRoute = require('./routes/imap');
 
-
+ const accessLogStream=fs.createWriteStream(path.join(__dirname,'access.log'),{flags:'a'}); 
 
 mongoose.connect( `mongodb+srv://meeranjianees1:Anees%4084@anees.r0egejs.mongodb.net/${process.env.DB_NAME}`);
 const db=mongoose.connection;
@@ -35,19 +38,13 @@ console.log('Database Connection Established!')
 
 // Create an Express app
 
-if(app.get('env')=="development"){
-  app.use(morgan('dev'));
 
-}
-
-
-
+ app.use(morgan('combined',{stream:accessLogStream}));
 
 app.use('/api',AuthRoute);
 app.use('/api',ImapRoute);
 app.use('/api',EmployeeRoute);
 app.use('/api',ProductRoute);
-
 
 
 // Start the server and listen on a specific port
